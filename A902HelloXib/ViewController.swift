acb2698e997999e6339698317c2d5868e28dd280
Danny Shen <shenfive@gmail.com>
2022-10-15 14:39:48 +0800

    Initial Commit

---
+++ A902HelloXib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A902HelloXib
+//
+//  Created by 申潤五 on 2022/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
